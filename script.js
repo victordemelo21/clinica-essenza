ebb50eed0728887ed9c2b70855a876f052d352bc
Victor de Melo <vdemeloaraujo@gmail.com>
2026-06-02 19:23:06 -0300

    changed text to look less promotional and more professional presenting

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const menuButton = document.querySelector('.menu-btn');
-const navLinks = document.querySelector('.nav-links');
+const menuButton = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
 
-menuButton?.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+menuButton?.addEventListener("click", () => {
+  navLinks?.classList.toggle("active");
 });
 
-document.querySelectorAll('.nav-links a').forEach((link) => {
-  link.addEventListener('click', () => navLinks.classList.remove('active'));
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks?.classList.remove("active");
+  });
 });
